097b22bb8c16f1b3ce95bbbeb1006f8e199a913f
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-11-14 19:33:01 +0300

    Shell

--- memories.c
+++ memories.c
@@ -1,9 +1,7 @@
 #include "shell.h"
-
 /**
  * bfree - frees a pointer and NULLs the address
  * @ptr: address of the pointer to free
- *
  * Return: 1 if freed, otherwise 0.
  */
 int bfree(void **ptr)
