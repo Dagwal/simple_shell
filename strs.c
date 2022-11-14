728a4ce0d0ec61f322371a0a48dd2873479635fc
Dagwal <108694236+Dagwal@users.noreply.github.com>
2022-11-14 19:32:09 +0300

    Updated

--- strs.c
+++ strs.c
@@ -3,7 +3,6 @@
  * _strcpy - copies a string
  * @dest: the destination
  * @src: the source
- *
  * Return: pointer to destination
  */
 char *_strcpy(char *dest, char *src)
@@ -24,8 +23,7 @@ char *_strcpy(char *dest, char *src)
 /**
  * _strdup - duplicates a string
  * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
+ * Return: pointer to the duplicated strings
  */
 char *_strdup(const char *str)
 {
@@ -46,7 +44,6 @@ char *_strdup(const char *str)
 /**
  *_puts - prints an input string
  *@str: the string to be printed
- *
  * Return: Nothing
  */
 void _puts(char *str)
@@ -65,9 +62,8 @@ void _puts(char *str)
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 
  */
 int _putchar(char c)
 {
